601223a8a4f119696f20abeafac958dd83bed609
VishalReddy3105 <tvishalreddy2003@gmail.com>
2023-01-05 22:30:34 +0530

    Updates

---
+++ precache-manifest.ad9cf7d0505b949d051628b2317212ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d2ccf2ddf1d9f58ce5e",
+    "url": "/contact-app/static/css/main.234c839c.chunk.css"
+  },
+  {
+    "revision": "4d2ccf2ddf1d9f58ce5e",
+    "url": "/contact-app/static/js/main.4d2ccf2d.chunk.js"
+  },
+  {
+    "revision": "ee91b39aa9af3544c0d5",
+    "url": "/contact-app/static/js/1.ee91b39a.chunk.js"
+  },
+  {
+    "revision": "6550abcc26af0c8da41d",
+    "url": "/contact-app/static/js/runtime~main.6550abcc.js"
+  },
+  {
+    "revision": "fcfe2bd1975fa2db1ff60aa0ccdd2349",
+    "url": "/contact-app/static/media/user.fcfe2bd1.jpg"
+  },
+  {
+    "revision": "e520b9f056bfd72a93bc4cf94cca5a1c",
+    "url": "/contact-app/index.html"
+  }
+];
